5a79f356a895154a8227ec09e4a628798159c5ea
BOYEONJEONG <qhdus96@naver.com>
2022-03-28 23:55:05 +0900

    N1697_init

---
+++ Graph/N1697.java
@@ -0,0 +1,14 @@
+//가장 빠른 시간
+//x+1, x-1, 2*
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class N1697 {
+
+    public static void main(String args[]){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+    }
+}
